7f17b4b1181d06248ea9ad34a411d21a2b1bdd00
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-07-15 21:01:47 +0300

    add ServiceConfigurator project

---
+++ ConfigGenerator/UserServiceConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.StorageSystem.Concrete.Services;
+namespace ConfigGenerator
+{
+    public class UserServiceConfigurator
+    {
+        public static IEnumerable<UserService> GenerateUserServices()
+        {
+            // read app config and create services
+            throw new NotImplementedException();
+        }
+    }
+}
