a7bc62f74abd1ce6d0c4c9c35cda2bcbc4692df7
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-07-19 19:55:56 +0300

    add Day5 solution

---
+++ Day5_Primitives/Monitor/AnotherClass.cs
@@ -0,0 +1,30 @@
+﻿namespace Monitor
+{
+    // TODO: Use SpinLock to protect this structure.
+    public class AnotherClass
+    {
+        private int _value;
+
+        public int Counter
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public void Increase()
+        {
+            _value++;
+        }
+
+        public void Decrease()
+        {
+            _value--;
+        }
+    }
+}
---
+++ Day5_Primitives/Monitor/MyClass.cs
@@ -0,0 +1,30 @@
+﻿namespace Monitor
+{
+    // TODO: Use Monitor (not lock) to protect this structure.
+    public class MyClass
+    {
+        private int _value;
+
+        public int Counter
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public void Increase()
+        {
+            _value++;
+        }
+
+        public void Decrease()
+        {
+            _value--;
+        }
+    }
+}
